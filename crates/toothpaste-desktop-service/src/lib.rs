796f8b550d50f8d2a4d993f03522c5ddae6bbecf
Brisk4t <atharvapcat@gmail.com>
2026-04-23 21:45:58 -0600

    Add service uuid based filter

--- crates/toothpaste-desktop-service/src/lib.rs
+++ crates/toothpaste-desktop-service/src/lib.rs
@@ -1,8 +1,14 @@
 use std::time::Duration;
 use tokio::time;
+use uuid::{uuid, Uuid};
 
 use btleplug::api::{Central, Manager as _, Peripheral, ScanFilter};
 use btleplug::platform::Manager;
+
+const SERVICE_UUID: Uuid                    = uuid!("19b10000-e8f2-537e-4f6c-d104768a1214");
+const PACKET_CHARACTERISTIC_UUID: Uuid      = uuid!("6856e119-2c7b-455a-bf42-cf7ddd2c5907");
+const HID_SEMAPHORE_CHARACTERISTIC_UUID: Uuid = uuid!("6856e119-2c7b-455a-bf42-cf7ddd2c5908");
+const MAC_ADDRESS_CHARACTERISTIC_UUID: Uuid = uuid!("19b10002-e8f2-537e-4f6c-d104768a1214");
 
 pub async fn ble_scan() -> Result<Vec<String>, btleplug::Error> {
     let manager = Manager::new().await?;
@@ -15,14 +21,17 @@ pub async fn ble_scan() -> Result<Vec<String>, btleplug::Error> {
 
     let mut discovered_devices = Vec::new();
     for adapter in adapter_list.iter() {
+        // Directly filtering by service UUID doesn't work reliably
         adapter.start_scan(ScanFilter::default()).await.expect("Failed to start scan");
         time::sleep(Duration::from_secs(5)).await;
 
         let peripherals = adapter.peripherals().await?;
         for peripheral in peripherals.iter() {
             let properties = peripheral.properties().await?;
-            if let Some(local_name) = properties.unwrap().local_name {
-                discovered_devices.push(local_name);
+            if let Some(props) = properties {
+                if props.services.contains(&SERVICE_UUID) {
+                    discovered_devices.push(props.local_name.unwrap_or_default());
+                }
             }
         }
     }
